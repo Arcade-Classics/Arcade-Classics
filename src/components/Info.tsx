d5bd586aa603f6fc8235cb9b066498dc619ff665
Hugo Maxted <hugo@maxtedfamily.net>
2022-12-14 08:10:00 +0000

    Fixed links

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,15 +1,15 @@
 const Info: () => JSX.Element = (): JSX.Element => {
   return (
     <div className="info">
-      <a href="https://www.arcade-classics.github.io/changelog" target="_blank" rel="noreferrer">
+      <a href="https://arcade-classics.github.io/changelog" target="_blank" rel="noreferrer">
         Version 2.1.4
       </a>
       <br />
-      <a href="https://www.arcade-classics.github.io/support" target="_blank" rel="noreferrer">
+      <a href="mailto:thearcadeclassics@gmail.com?subject=Arcade Classics Support Request" target="_blank" rel="noreferrer">
         Support
       </a>
       <br />
-      <a href="https://www.arcade-classics.github.io/credits" target="_blank" rel="noreferrer">
+      <a href="https://arcade-classics.github.io/credits" target="_blank" rel="noreferrer">
         Credits
       </a>
     </div>
